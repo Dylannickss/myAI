7e362041d0635d503bbbb61516affd0ef3a62026
Dylan Nicks <111459712+Dylannickss@users.noreply.github.com>
2025-03-17 14:56:47 -0400

    Update footer.tsx

--- components/chat/footer.tsx
+++ components/chat/footer.tsx
@@ -31,31 +31,3 @@ export default function ChatFooter() {
   );
 }
 
-
-/*export default function ChatFooter() {
-  return (
-    <div className="w-full text-xs flex pt-2 text-gray-500">
-      <div className="flex-grow text-left">
-        {/* Left Pane */}
-        <Link href="/terms" className="hover:underline">
-          Terms of Service
-        </Link>
-      </div>
-      <div className="flex-grow text-center">
-        {/* Center Pane */}
-        {FOOTER_MESSAGE}
-      </div>
-      <div className="flex-grow text-right">
-        {/* Right Pane */}
-        {/* Do not remove or modify the following message. Removal or modification violates the license agreement. */}
-        <a
-          href="http://www.ringel.ai"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          powered by ringel.AI
-        </a>
-      </div>
-    </div>
-  );
-}*/
